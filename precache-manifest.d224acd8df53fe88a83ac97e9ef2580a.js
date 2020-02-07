c111578179c043e75ad35688da60880eb2f12a73
Akash Temburnikar <akashappa1110@gmail.com>
2020-02-07 15:32:28 -0500

    Updates

---
+++ precache-manifest.d224acd8df53fe88a83ac97e9ef2580a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93a582a9e11e6fa3a997d6d1fdd7c87c",
+    "url": "/Akash-MyPortfolio/index.html"
+  },
+  {
+    "revision": "70e49ef07d51f789ec0f",
+    "url": "/Akash-MyPortfolio/static/css/main.5303d68a.chunk.css"
+  },
+  {
+    "revision": "c28aa048bb63a96c160f",
+    "url": "/Akash-MyPortfolio/static/js/2.859e7020.chunk.js"
+  },
+  {
+    "revision": "c949ead957a8a8fec77d7e9e321e9f66",
+    "url": "/Akash-MyPortfolio/static/js/2.859e7020.chunk.js.LICENSE"
+  },
+  {
+    "revision": "70e49ef07d51f789ec0f",
+    "url": "/Akash-MyPortfolio/static/js/main.47173455.chunk.js"
+  },
+  {
+    "revision": "745bd761ab6f3f242922",
+    "url": "/Akash-MyPortfolio/static/js/runtime-main.1ec1321a.js"
+  }
+]);
